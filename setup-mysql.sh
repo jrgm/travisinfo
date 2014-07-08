b236795e91ab615bfbcb14c0b910953e8296235e
John Morrison <jrgmorrison@gmail.com>
2014-07-07 21:25:28 -0700

    set

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -o errexit
+set -o xtrace
+set -o nounset
+
+# https://gist.github.com/drogus/6718448
 
 apt-get remove mysql-common mysql-server-5.5 mysql-server-core-5.5 mysql-client-5.5 mysql-client-core-5.5
 apt-get autoremove
@@ -8,6 +14,7 @@ dpkg -i mysql-5.6.14.deb
 cp /opt/mysql/server-5.6/support-files/mysql.server /etc/init.d/mysql.server
 ln -s /opt/mysql/server-5.6/bin/* /usr/bin/
 # some config values were changed since 5.5
+cat /etc/mysql/my.cnf
 sed -i'' 's/table_cache/table_open_cache/' /etc/mysql/my.cnf
 sed -i'' 's/log_slow_queries/slow_query_log/' /etc/mysql/my.cnf
 sed -i'' 's/basedir[^=]\\+=.*$/basedir = \\/opt\\/mysql\\/server-5.6/' /etc/mysql/my.cnf
